580f9b1a312fd0ad893095d0c13fcb536cfebdb3
Auke Willem Oosterhoff <awoosterhoff@gmail.com>
2014-11-23 23:51:36 +0100

    #7 Refactored test.

--- app/event_dispatcher_test.go
+++ app/event_dispatcher_test.go
@@ -5,77 +5,60 @@ import (
 )
 
 var ed EventDispatcher
+var isCalled bool
+var done = make(chan bool)
 
-func init() {
+var f = func(emsg *EventMessage) *EventMessage {
+	isCalled = true
+	done <- true
+
+	return emsg
+}
+
+func setUp() {
 	ed = EventDispatcher{
 		callbacks: make(map[string][]func(*EventMessage) *EventMessage),
 	}
+	isCalled = false
 }
 
 func TestBind(t *testing.T) {
-	someEvent := func(e *EventMessage) (emsg *EventMessage) {
-		return e
-	}
-
-	someOtherEvent := func(e *EventMessage) (emsg *EventMessage) {
-		return e
-	}
-
-	ed.Bind("some_event", someEvent)
-	ed.Bind("some_event", someOtherEvent)
+	setUp()
+	ed.Bind("some_event", f)
+	ed.Bind("some_event", f)
 
 	if len(ed.callbacks["some_event"]) != 2 {
 		t.Errorf("EventDispatcher should have 2 callbacks bound to 'some_event' got %v", len(ed.callbacks["some_event"]))
 	}
 }
 
-func TestDispatch(t *testing.T) {
-	msg := EventMessage{Event: "some_event"}
-	chnl := make(chan bool)
-
-	isCalled := false
-	someEvent := func(e *EventMessage) (emsg *EventMessage) {
-		isCalled = true
-		chnl <- true
-
-		return e
-	}
-
-	ed.Bind("some_event", someEvent)
-	ed.Dispatch(&msg)
-
-	// Wait for the someEvent to be called.
-	<-chnl
-
-	if isCalled == false {
-		t.Error("Method has not been dispatched.")
-	}
-}
-
 func TestListen(t *testing.T) {
+	setUp()
 	msg := `{"event": "some_event", "data": {"pin_id": 1, "state": 0}}`
-	isExecuted := false
-	done := make(chan bool)
 	messages := make(chan string, 1)
 
-	someEvent := func(e *EventMessage) (emsg *EventMessage) {
-		isExecuted = true
-		done <- true
-		return e
-	}
-
-	ed.Bind("some_event", someEvent)
+	ed.Bind("some_event", f)
 	go ed.Listen(messages)
 	messages <- msg
 
 	<-done
 
-	if isExecuted == false {
+	if isCalled == false {
 		t.Error("someEvent has not been dispatched.")
 	}
-
-	close(messages)
 }
 
-// Global EventDispatcher instance
-// TestListen
+func TestDispatch(t *testing.T) {
+	setUp()
+	msg := EventMessage{Event: "some_event"}
+
+	ed.Bind("some_event", f)
+	ed.Dispatch(&msg)
+
+	// Wait for the someEvent to be called.
+	<-done
+
+	if isCalled == false {
+		t.Error("Method has not been dispatched.")
+	}
+}
